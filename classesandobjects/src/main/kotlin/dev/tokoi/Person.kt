b58b7113aa9c5996d94812cce471153acdf828b7
Wesley Tokoi <tokoiwesley@gmail.com>
2021-10-20 22:23:59 +0300

    Add a secondary constructor

--- classesandobjects/src/main/kotlin/dev/tokoi/Person.kt
+++ classesandobjects/src/main/kotlin/dev/tokoi/Person.kt
@@ -9,17 +9,23 @@ interface Signatory {
 open class Person(val name: String, var age: Int) : Signatory {
 
     init {
-        if (name == "Wesley" && age < 31) throw Exception("Invalid age")
+        if (name == "Wesley" && age < 20) throw Exception("Invalid age")
     }
 
-    override fun sign() = println("$name aged $age can sign documents")
+    var isMarried: Boolean = false;
+
+    constructor(name: String, age: Int, isMarried: Boolean) : this(name, age) {
+        this.isMarried = isMarried
+    }
+
+    override fun sign() = println("$name aged $age can sign documents (and their marital status is $isMarried)")
 
 }
 
 class Student(name: String, age: Int) : Person(name, age)
 
 fun main(args: Array<String>) {
-    val p = Person("Wesley", 21)
+    val p = Person("Wesley", 21, true)
     p.sign()
 
     p.age = 31
