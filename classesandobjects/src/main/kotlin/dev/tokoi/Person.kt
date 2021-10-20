a280422fec562cc681cb2dc129ce948688c97873
Wesley Tokoi <tokoiwesley@gmail.com>
2021-10-20 22:41:40 +0300

    Add a static method in a companion object

--- classesandobjects/src/main/kotlin/dev/tokoi/Person.kt
+++ classesandobjects/src/main/kotlin/dev/tokoi/Person.kt
@@ -10,6 +10,17 @@ open class Person(val name: String, var age: Int, var isMarried: Boolean = false
 
     override fun sign() = println("$name aged $age can sign documents (and their marital status is $isMarried)")
 
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val p = Person("Wesley", 21, true)
+            p.sign()
+
+            p.age = 31
+            p.sign()
+        }
+    }
+
 }
 
 class Student(name: String, age: Int) : Person(name, age)
