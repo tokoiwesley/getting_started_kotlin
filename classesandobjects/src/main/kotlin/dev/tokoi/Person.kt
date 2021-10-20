99aca1addf623a681e7594af4e4f4a3f3d18477b
Wesley Tokoi <tokoiwesley@gmail.com>
2021-10-20 22:57:41 +0300

    Copy a data object

--- classesandobjects/src/main/kotlin/dev/tokoi/Person.kt
+++ classesandobjects/src/main/kotlin/dev/tokoi/Person.kt
@@ -41,6 +41,9 @@ fun main(args: Array<String>) {
 
     val wesley = User("Wesley", 1)
     println(wesley)
+
     val (name, id) = wesley
     println("User is $name and their id is $id")
+    val otherUser = wesley.copy(id = 2)
+    println(otherUser)
 }
